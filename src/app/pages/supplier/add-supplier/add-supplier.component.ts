fa4270dee6fd63b5c33a0c83550970ef410ddc0d
glynn315 <94496602+glynn315@users.noreply.github.com>
2025-07-15 22:16:11 +0800

    Implement Supplier Modal

--- src/app/pages/supplier/add-supplier/add-supplier.component.ts
+++ src/app/pages/supplier/add-supplier/add-supplier.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ModalComponent } from "../../../shared/modal/modal.component";
 
 @Component({
   selector: 'app-add-supplier',
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './add-supplier.component.html',
   styleUrl: './add-supplier.component.scss'
 })
 export class AddSupplierComponent {
+  @Input() openSupplierModal = false;
+  headerText = 'Add New Supplier';
+  @Output() openSupplierModalChange = new EventEmitter<boolean>();
 
+  closeModal() {
+    this.openSupplierModalChange.emit(false);
+  }
 }
--- src/app/pages/supplier/supplier.component.ts
+++ src/app/pages/supplier/supplier.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AddSupplierComponent } from "./add-supplier/add-supplier.component";
+import { LucideAngularModule ,PlusCircleIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-supplier',
-  imports: [],
+  imports: [AddSupplierComponent, LucideAngularModule],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.scss'
 })
 export class SupplierComponent {
-
+  readonly plus = PlusCircleIcon;
+  openSupplierModal = false;
+  openModal(){
+    this.openSupplierModal = true;
+  }
+  closeModal() {
+    this.openSupplierModal = false;
+  }
 }
